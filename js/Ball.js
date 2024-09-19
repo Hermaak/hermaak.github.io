1668a5a757443bc58728d6e3bb633d663d437271
sairessy <sairessy@gmail.com>
2024-09-19 11:42:16 -0700

    Nice

--- js/Ball.js
+++ js/Ball.js
@@ -5,11 +5,7 @@ class Ball {
     this.y = y;
     this.r = random(4, 12);
     this.speed = random(0.5, 1.5).toFixed(2);
-    // this.color = "#FF174455";
-    // this.color = "#001744ff";
-    // this.color = '#ff0000';
     this.color = new p5.Vector(parseInt(random(255)), parseInt(random(255)), parseInt(random(255)));
-    this.sci = Math.round(Math.random() * (scientists.length - 1));
   }
 
   init() {
@@ -19,16 +15,6 @@ class Ball {
     noStroke();
     ellipse(this.x, this.y, this.r, this.r);
     pop();
-    // line(this.x, this.y, this.x + 50, this.y - 150);
-    // line(this.x + 50, this.y - 150, this.x + 100, this.y - 150);
-    // stroke(this.color);
-    // text(
-    //   `${scientists[this.sci].name} (${round(this.y)})`,
-    //   this.x + 110,
-    //   this.y - 120
-    // );
-    // text(`🏳 ${scientists[this.sci].origin})`, this.x + 110, this.y - 90);
-    // rect(this.x + 100, this.y - 150, 200, 80);
     this.fall();
   }
 
@@ -48,7 +34,6 @@ class Ball {
     this.y = -this.r;
     this.x = random(0, width);
     this.r = random(2, 10);
-    this.sci = Math.round(Math.random() * (scientists.length - 1));
 
   }
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,25 +1,13 @@
-const lm = ["Criar", "Inovar", "Optimizar"];
-
 let balls = [];
 
-const scientists = [
-  { name: "Newton", origin: "England" },
-  { name: "Pascal", origin: "France" },
-  { name: "Heartz", origin: "France" },
-  { name: "Lagrange", origin: "Italy" },
-  { name: "Lavosier", origin: "France" },
-  { name: "Tesla", origin: "France" },
-  { name: "Leibniz", origin: "German" },
-];
 
 let canvas,
-  sound,
   button,
   slider,
   span,
   vol = 0.09;
 
-  let intro;
+let intro;
 
 let score = 0;
 const num_balls = 10;
@@ -28,117 +16,26 @@ let end_game = true;
 let player;
 
 function preload() {
-  sound = loadSound("assets/sound/sia_snowflake.mp3");
-  intro = loadImage('assets/img/intro.svg');
+  intro = loadImage("assets/img/intro.svg");
 }
 
 function setup() {
   canvas = createCanvas(innerWidth, innerHeight + 100);
   canvas.parent(document.querySelector(".sketch"));
-  button = createButton("<i class='la la-play'></i>");
-  button.position(width - 100, height * 0.5);
-
-  slider = createSlider(0, 1, vol, 0.01);
-  slider.position(width - 135, height * 0.5 - 100);
-  slider.style("cursor: pointer");
-  slider.style("accent-color: #9f34c0");
-  slider.style("transform: rotate(-90deg)");
-  slider.style("width: 100px");
-  slider.style("height: 6px");
-  slider.style("z-index: 5");
-
-  span = createSpan("Sia - Snowflake");
-  span.position(width - 130, height * 0.5 + 100);
-  span.style("color: #9f34c0");
-  span.style("transform: rotate(90deg)");
-
-  sound.setLoop(true);
-  sound.setVolume(vol);
-
-  button.mousePressed(mp);
 
   for (var i = 0; i < num_balls; i++) {
     balls.push(new Ball(random(0, width), random(-500, -100), i));
   }
-
-  player = new Player(width * .5, height - height * .2);
 }
 
 function draw() {
   background("#111");
-  // player.show();
-  // push();
-  // fill(255);
-  // text(score + " / " + num_balls, mouseX, mouseY);
-  // stroke(255, 0, 0);
-  // noFill();
-  // ellipse(mouseX, mouseY, 50, 50);
-  // pop();
   imageMode(CENTER);
-  image(intro, width * .5, height * .5, intro.width / 2,  intro.height / 2)
-
+  image(intro, width * 0.5, height * 0.5, intro.width / 2, intro.height / 2);
 
   balls.forEach((ball) => {
     ball.init();
-    const d = dist(ball.x, ball.y, player.x, player.y)
-    if(d < 10) {
-      player.isDamaged = true;
-      setTimeout(() => {
-        player.isDamaged = false;
-      }, 1000);
-    }
   });
-
-  vol = slider.value();
-
-  // if (!end_game) {
-  //   if (balls.length !== 0) {
-  //     balls.forEach((ball) => {
-  //       const d = dist(ball.x, ball.y, mouseX, mouseY);
-  //       if (d < 20) {
-  //         balls = balls.filter(({ id }) => id !== ball.id);
-  //         score++;
-  //       }
-  //     });
-  //   } else {
-  //     alert(`Pontuação: ${score} / ${num_balls}`);
-  //     score = 0;
-  //     const p = prompt("Introduza [Y] para jogar novamente.");
-  //     if (p.toLowerCase() === "y") {
-  //       for (var i = 0; i < num_balls; i++) {
-  //         balls.push(new Ball(random(0, width), random(-500, -100), i));
-  //       }
-  //     } else {
-  //       end_game = true;
-  //     }
-  //   }
-  // }
-
-  sound.setVolume(vol);
-
-  fill("#9f34c0");
-  textSize(16);
-  text(round(vol * 100) + "%", width - 95, height * 0.5 - 200);
-
-  // if(keyIsPressed) {
-  //   if(key === 'ArrowLeft') {
-  //     player.x -= player.speed;
-  //   }
-  
-  //   if(key === 'ArrowRight') {
-  //     player.x += player.speed;
-  //   }
-  // }
-}
-
-function mp() {
-  if (!sound.isPlaying()) {
-    sound.play();
-    button.html("<i class='la la-stop'></i>");
-  } else {
-    sound.stop();
-    button.html("<i class='la la-play'></i>");
-  }
 }
 
 function mouseDragged() {
